4cfe4878f7262f5932586ca9b3f716fc720079a9
พงศธร อุ่นตาล <pongsatorn.u@rtarf.mi.th>
2022-09-29 23:43:23 +0700

    Read Quest

--- routes/auth.py
+++ routes/auth.py
@@ -90,6 +90,7 @@ def create_access_token(data: dict, expires_delta: timedelta | None = None):
 
 
 async def get_current_user(token: str = Depends(oauth2_scheme)):
+    print("KKK")
     credentials_exception = HTTPException(
         status_code=status.HTTP_401_UNAUTHORIZED,
         detail="Could not validate credentials",
--- sql/student.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO student (std_id, sec_id)
-VALUE (1, 4), (2, 4), (3, 4);
-
-DELETE FROM student
